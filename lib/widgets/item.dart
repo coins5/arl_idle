3ac686c341bf7cf89000d5695ceedf61ac748b6b
Marlon Colca <marlon.colca.io@gmail.com>
2025-06-08 07:38:57 -0500

    arl

--- lib/widgets/item.dart
+++ lib/widgets/item.dart
@@ -1,20 +1,7 @@
 import 'package:flutter/material.dart';
-import 'dart:async';
 
-class Item extends StatelessWidget {
-  const Item({super.key});
-
-  Timer? timer;
-
-  void startTimer() {
-    timer = Timer.periodic(Duration(seconds: 1), (Timer t) {
-      print("Este mensaje aparece cada segundo");
-    });
-  }
-
-  void stopTimer() {
-    timer?.cancel();
-  }
+class ActivityWidget extends StatelessWidget {
+  const ActivityWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
